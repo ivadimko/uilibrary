6edefafcc09a7f18dc25100381f1b4dcdbb4e8c0
Vadim Ilchenko <vvadimko@gmail.com>
2017-09-05 13:03:38 +0300

    add new items

--- dev/assets/scripts/common.js
+++ dev/assets/scripts/common.js
@@ -1,42 +1,57 @@
-jQuery(document).ready(function ($) {
-//--------------Buttons----------------
-//--------Buttons hide heading---------
-$(document).on('click', '.button', function () {
+document.addEventListener("DOMContentLoaded", function (event) {
+	mobileNav();
+	btnShowClass();
+});
+window.onload = function() {
 	
-	$('.item__heading').slideUp();
-	$(this).prev('.item__heading').slideToggle();
-	return false;
-});
-//-------------------------------------
-
-//-------------------------------------
-
-
-});
-jQuery(window).on('load', function () {
-	mobileNav.init('.header__menu');
-});
-
-var mobileNav = {
-	className: '.js_mobile-nav',
-	mobileMenuClassName: '.mobile-nav__menu',
-	activeClass: 'open',
-	init: function (mainMenuClassName) {
-		if (!$(this.mobileMenuClassName).children().length) {
-			$(mainMenuClassName).children().clone().prependTo(this.mobileMenuClassName);
-		}
-
-		$(document).on('click', '.burger', function () {
-			mobileNav.toggle();
-		});
-	},
-	open: function () {
-		$(this.className).addClass(this.activeClass);
-	},
-	close: function () {
-		$(this.className).removeClass(this.activeClass);
-	},
-	toggle: function () {
-		$(this.className).hasClass(this.activeClass) ? this.close() : this.open();
-	}
 };
+//----------Mobile menu function----------
+function mobileNav() {
+		var menu 			= document.querySelector('.header__menu'),
+			mobilemenu 		= document.querySelector('.mobile-nav__menu'),
+			JSinit 			= document.querySelector('.js_mobile-nav'),
+			burger 			= document.querySelectorAll('.burger'),
+			activeClass 	= 'open';
+		function toggle() {
+			JSinit.classList.toggle(activeClass);
+		}
+		if (mobilemenu.innerHTML == 0)
+			mobilemenu.innerHTML = menu.innerHTML;
+	
+		/* for (var i = 0; i < burger.length; i++) {
+				burger[i].addEventListener('click', toggle);
+			} */
+		for (var i = 0; i < burger.length; i++) {
+			burger[i].onclick = toggle;
+			};
+}
+//----------------------------------------
+
+//----------------Buttons-----------------
+//--------Buttons show Class---------
+function btnShowClass() {
+	var btn 		= document.querySelectorAll('.button'),
+		btnHead		= document.createElement('p');
+	for (var i = 0; i < btn.length; i++) {
+		btn[i].onclick = function() {
+			
+			//Show ClassList
+			btnHead.classList.add('item__heading');
+			btnHead.innerHTML = (this.classList.value);
+			this.parentNode.insertBefore(btnHead, this);
+			
+			//Select button classes  
+			window.getSelection().removeAllRanges();  
+			var range = document.createRange();  
+			range.selectNode(btnHead);  
+			window.getSelection().addRange(range);  			  
+			
+			//link cancelattion
+			return false;
+			
+		}
+	};
+
+}
+//-----------------------------------
+//----------------------------------------
